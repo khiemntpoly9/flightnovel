85b739bf794981ba51cf622d069440ff0794b487
Nguyễn Trung Khiêm <trungkhiem1412@gmail.com>
2023-09-28 18:43:14 +0700

    fix migration user

--- database/migrations/2023_09_20_055641_users.php
+++ database/migrations/2023_09_20_055641_users.php
@@ -14,7 +14,7 @@ return new class extends Migration {
 			$table->bigIncrements('id');
 			$table->string('name');
 			$table->string('email')->unique();
-			$table->timestamp('email_verified_at')->nullable();
+			$table->dateTime('email_verified_at')->nullable();
 			$table->string('password')->nullable();
 			$table->integer('id_role')->unsigned()->default(2);
 			$table->string('provider')->nullable();
