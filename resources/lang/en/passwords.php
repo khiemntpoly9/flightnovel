f1feee688a310ef917b6fdc812497b0b74c37484
Nguyễn Trung Khiêm <trungkhiem1412@gmail.com>
2023-10-01 19:38:48 +0700

    update khôi phục mật khẩu

---
+++ resources/lang/en/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Your password has been reset.',
+    'sent' => 'Chúng tôi đã gửi cho bạn Email xác thực khôi phục.',
+    'throttled' => 'Please wait before retrying.',
+    'token' => 'This password reset token is invalid.',
+    'user' => "Email này chưa được đăng ký.",
+
+];
