6d181eaeb447b29fa83b92e48fb698ea1bd5daaa
hoaiphuong97 <131567082+hoaiphuong97@users.noreply.github.com>
2023-10-06 21:36:13 +0700

    novel-team

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -20,12 +20,12 @@ class TeamController extends Controller
 		if (!$team_user) {
 			return Inertia::render('Client/Team/Team', [
 				'team_user' => $team_user,
-				'success' => $success,
+				'success' => $success
 			]);
 		} else {
 			// Lấy novel có id_team = id của team
 			$team = TeamUser::with('team')->where('id_user', auth()->user()->id)->first();
-			$novel = Novel::where('id_team', $team->id)->get();
+			$novel = Novel::where('id_team', $team->id_team)->get();
 			return Inertia::render('Client/Team/Team', [
 				'team_user' => $team_user,
 				'team' => $team,
--- resources/js/Pages/Client/Team/Team.jsx
+++ resources/js/Pages/Client/Team/Team.jsx
@@ -35,23 +35,97 @@ export default function Team({ auth, team_user, team, novel, success }) {
 		return (
 			<DefaultLayout auth={auth}>
 				<div className='container mx-auto w-10/12'>
-					<div>
-						Tên nhóm: <span>{team.team.team_name}</span> <br />
-						<div dangerouslySetInnerHTML={{ __html: `${team.team.team_detail}` }}></div>
+					<h1 className='text-center font-bold text-3xl p-2 m-4'>Thông tin nhóm</h1>
+					<div className='mb-5'>
+						<strong>Tên nhóm: </strong> <span>{team.team.team_name}</span> <br />
+						<strong>Chi tiết: </strong> <span dangerouslySetInnerHTML={{ __html: `${team.team.team_detail}` }}></span>
+					</div>
+					
+					{/* Show truyện */}
+					<div className='mt-2'>
+						<span className='font-bold text-xl'>Danh sách truyện</span>
+						<div className='bottom relative grid grid-cols-3 gap-2 py-2 sm:grid-cols-4 lg:grid-cols-6 mb-4'>
+							{novel.map((novel) => (
+								<>
+									<div className='card'>
+										<img
+											src={novel.thumbnail}
+											alt=''
+											className=''
+										/>
+										<div className='name'>
+											<h2 className='font-semibold mt-2 text-xl text-center'>{novel.name_novel}</h2>
+											<p className='font-semibold mt-0'> Tác giả: {novel.author}</p>
+											<p className='font-semibold mt-0'> Họa sĩ: {novel.illustrator}</p>
+										</div>
+									</div>
+									<div className='card'>
+										<img
+											src={novel.thumbnail}
+											alt=''
+											className=''
+										/>
+										<div className='name'>
+											<h2 className='font-semibold mt-2 text-xl text-center'>{novel.name_novel}</h2>
+											<p className='font-semibold mt-0'> Tác giả: {novel.author}</p>
+											<p className='font-semibold mt-0'> Họa sĩ: {novel.illustrator}</p>
+										</div>
+									</div>
+									<div className='card'>
+										<img
+											src={novel.thumbnail}
+											alt=''
+											className=''
+										/>
+										<div className='name'>
+											<h2 className='font-semibold mt-2 text-xl text-center'>{novel.name_novel}</h2>
+											<p className='font-semibold mt-0'> Tác giả: {novel.author}</p>
+											<p className='font-semibold mt-0'> Họa sĩ: {novel.illustrator}</p>
+										</div>
+									</div>
+									<div className='card'>
+										<img
+											src={novel.thumbnail}
+											alt=''
+											className=''
+										/>
+										<div className='name'>
+											<h2 className='font-semibold mt-2 text-xl text-center'>{novel.name_novel}</h2>
+											<p className='font-semibold mt-0'> Tác giả: {novel.author}</p>
+											<p className='font-semibold mt-0'> Họa sĩ: {novel.illustrator}</p>
+										</div>
+									</div>
+									<div className='card'>
+										<img
+											src={novel.thumbnail}
+											alt=''
+											className=''
+										/>
+										<div className='name'>
+											<h2 className='font-semibold mt-2 text-xl text-center'>{novel.name_novel}</h2>
+											<p className='font-semibold mt-0'> Tác giả: {novel.author}</p>
+											<p className='font-semibold mt-0'> Họa sĩ: {novel.illustrator}</p>
+										</div>
+									</div>
+									<div className='card'>
+										<img
+											src={novel.thumbnail}
+											alt=''
+											className=''
+										/>
+										<div className='name'>
+											<h2 className='font-semibold mt-2 text-xl text-center'>{novel.name_novel}</h2>
+											<p className='font-semibold mt-0'> Tác giả: {novel.author}</p>
+											<p className='font-semibold mt-0'> Họa sĩ: {novel.illustrator}</p>
+										</div>
+									</div>
+								</>
+							))}
+						</div>
 					</div>
 					<Link className='btn' href={route('novel.index')}>
 						Thêm truyện
 					</Link>
-					{/* Show truyện */}
-					<div className='mt-2'>
-						<span>Danh sách truyện</span>
-						{novel.map((novel) => (
-							<div key={novel.id_novel}>
-								<img src={novel.thumbnail} alt='thumbnail' />
-								<p>{novel.name_novel}</p>
-							</div>
-						))}
-					</div>
 				</div>
 			</DefaultLayout>
 		);
