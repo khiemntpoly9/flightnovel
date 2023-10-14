41b45cdf17578202f2cda4aa75cb9401b61330c8
manden520 <92621988+manden520@users.noreply.github.com>
2023-10-14 08:36:33 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CateController;
 use App\Http\Controllers\ChapController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\NovelController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProviderController;
@@ -21,15 +22,23 @@ use Inertia\Inertia;
 | contains the "web" middleware group. Now create something great!
 |
 */
+// Home
+Route::get('/', [HomeController::class, 'HomeIndex'])->name('home');
 
-Route::get('/', function () {
-	return Inertia::render('Client/Home', [
-		'canLogin' => Route::has('login'),
-		'canRegister' => Route::has('register'),
-	]);
+// Novel Read User
+Route::prefix('novel')->group(function () {
+	Route::get('/{novel:slug}', [NovelController::class, 'NovelRead'])->name('novel.read');
+	// Route::get('/{novel:slug}/vol/{id_vol}', [VolController::class, 'VolRead'])->name('vol.read');
+	// Route::get('/{id}/vol/{id_vol}/chap/{id_chap}', [ChapController::class, 'ChapRead'])->name('chap.read');
 });
 
-// Profile
+// Follow
+Route::middleware('auth')->prefix('follow')->group(function () {
+	Route::post('/{id}', [NovelController::class, 'NovelFollow'])->name('novel.follow');
+	Route::delete('/{id}', [NovelController::class, 'NovelUnFollow'])->name('novel.unfollow');
+});
+
+// Profile - Auth Role
 Route::middleware('auth')->prefix('profile')->group(function () {
 	Route::get('/', [ProfileController::class, 'edit'])->name('profile.edit');
 	Route::get('/update', [ProfileController::class, 'updateAccount'])->name('profile.create');
@@ -37,55 +46,56 @@ Route::middleware('auth')->prefix('profile')->group(function () {
 	Route::post('/update/avatar', [ProfileController::class, 'updateAvatar'])->name('profile.avatar');
 });
 
-// Team
+// Team Role - Auth Role
 Route::middleware('auth')->prefix('team')->group(function () {
 	Route::get('/', [TeamController::class, 'TeamIndex'])->name('team.index');
-	Route::get('/novel/{id}', [TeamController::class, 'TeamNovel'])->middleware('team.user')->name('team.novel');
+	// Team Role
 	Route::middleware('team')->group(function () {
 		Route::get('/create', [TeamController::class, 'TeamCreate'])->name('team.create');
 		Route::post('/create', [TeamController::class, 'TeamStore'])->name('team.store');
 	});
-	// Team User
-	Route::middleware('team.user')->group(function () {
-		// Novel
-		Route::get('/novel/{id}/update', [NovelController::class, 'NovelUp'])->name('novel.update.page');
-		Route::patch('/novel/{id}/update', [NovelController::class, 'NovelUpdate'])->name('novel.update');
-		// Vol
-		Route::get('/novel/{id}/vol', [VolController::class, 'VolIndex'])->name('vol.index');
-		Route::get('/novel/{id}/vol/{id_vol}', [VolController::class, 'VolUpdatePage'])->name('vol.update.page');
-		Route::patch('/novel/{id}/vol/{id_vol}', [VolController::class, 'VolUpdate'])->name('vol.update');
-		Route::post('/novel/{id}/vol', [VolController::class, 'VolStore'])->name('vol.create');
-		Route::delete('/novel/{id}/vol/{id_vol}', [VolController::class, 'VolDelete'])->name('vol.delete');
-		// Chap
-		Route::get('/novel/{id}/vol/{id_vol}/create-chap', [ChapController::class, 'ChapCreate'])->name('chap.create');
-		Route::get('/novel/{id}/vol/{id_vol}/update-chap/{id_chap}', [ChapController::class, 'ChapUpdate'])->name('chap.update');
-		Route::patch('/novel/{id}/vol/{id_vol}/update-chap/{id_chap}', [ChapController::class, 'ChapUpdatePatch'])->name('chap.update.patch');
-		Route::post('/novel/{id}/vol/{id_vol}/create-chap', [ChapController::class, 'ChapStore'])->name('chap.store');
-		Route::delete('/novel/{id}/vol/{id_vol}/chap/{id_chap}', [ChapController::class, 'ChapDelete'])->name('chap.delete');
+	// Novel
+	Route::prefix('novel')->group(function () {
+		Route::get('/create', [NovelController::class, 'NovelIndex'])->name('novel.index');
+		Route::post('/create', [NovelController::class, 'NovelCreate'])->name('novel.create');
+		// Team User Role
+		Route::middleware('team.user')->group(function () {
+			Route::get('/{novel:slug}', [TeamController::class, 'TeamNovel'])->name('team.novel');
+			Route::prefix('{novel:slug}')->group(function () {
+				Route::get('/create-vol', [VolController::class, 'VolIndex'])->name('vol.index');
+				Route::post('/create-vol', [VolController::class, 'VolStore'])->name('vol.create');
+				// Vol
+				Route::prefix('{vol:slug}')->group(function () {
+					Route::get('/edit', [VolController::class, 'VolUpdatePage'])->name('vol.update.page');
+					Route::patch('/update', [VolController::class, 'VolUpdate'])->name('vol.update');
+					Route::delete('/delete', [VolController::class, 'VolDelete'])->name('vol.delete');
+					// Chap
+					Route::get('/create-chap', [ChapController::class, 'ChapCreate'])->name('chap.create');
+					Route::post('/create-chap', [ChapController::class, 'ChapStore'])->name('chap.store');
+					Route::prefix('{chap:slug}')->group(function () {
+						Route::get('/edit', [ChapController::class, 'ChapUpdate'])->name('chap.update');
+						Route::patch('/update', [ChapController::class, 'ChapUpdatePatch'])->name('chap.update.patch');
+						Route::delete('/delete', [ChapController::class, 'ChapDelete'])->name('chap.delete');
+					});
+				});
+			});
+		});
 	});
-
 });
 
-// Novel
-Route::middleware('auth')->prefix('novel')->group(function () {
-	Route::get('/', [NovelController::class, 'NovelIndex'])->name('novel.index');
-	Route::post('/', [NovelController::class, 'NovelCreate'])->name('novel.create');
-	Route::get('/novel/{id}/update', [NovelController::class, 'NovelUp'])->name('novel.up');
-	
-});
-
-// Admin
+// Admin Role
 Route::middleware('admin')->prefix('admin')->group(function () {
 	Route::get('/', function () {
 		return Inertia::render('Admin/AdminMain');
 	})->name('admin.home');
 	Route::get('/categories', [CateController::class, 'CateIndex'])->name('admin.categories');
-	Route::get('/team', [TeamController::class, 'TeamAdmin'])->name('admin.team');
-	Route::get('/novel', [NovelController::class, 'NovelAdmin'])->name('admin.novel');
 	Route::post('/categories', [CateController::class, 'CateStore'])->name('admin.categories.store');
 	Route::patch('/categories', [CateController::class, 'CateUpdate'])->name('admin.categories.update');
 	Route::delete('/categories/{id}', [CateController::class, 'CateDelete'])->name('admin.categories.delete');
 	Route::get('/categories/detail/{id}', [CateController::class, 'CateDetail'])->name('admin.categories.detail');
+	Route::get('/team', [TeamController::class, 'TeamAdmin'])->name('admin.team');
+	Route::get('/team/detail/{id}', [TeamController::class, 'TeamDetailAdmin'])->name('admin.teamdetail');
+	Route::get('/novel', [NovelController::class, 'NovelAdmin'])->name('admin.novel');
 });
 
 // Login Provider
