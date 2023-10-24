3a6b37c7da6b8fa417a1acb3645e2add0e2f8d30
quoc <108472981+kensoonv2@users.noreply.github.com>
2023-10-24 14:57:12 +0700

    update

--- resources/js/Pages/Client/Search/Search.jsx
+++ resources/js/Pages/Client/Search/Search.jsx
@@ -48,109 +48,112 @@ export default function Search({ auth }) {
 					</ul>
 				</div>
 				{/* End Breadcrumbs */}
-				{/* {seacrh} */}
-				<div className='relative mt-2 flex flex-col'>
-					<div className='flex w-full'>
-						<input
-							type='text'
-							placeholder='Tên Tác Phẩm...'
-							className=' w-full flex-1 rounded-l-md border px-4  py-2 text-sm outline-none md:py-3  md:text-base'
-						/>
-						<button
-							type='submit'
-							className='flex-grow-0 rounded-r-md bg-green-500 px-4 py-2 text-sm font-bold text-white  hover:bg-green-600  md:text-base'
-						>
-							Tìm kiếm
-						</button>
-					</div>
-				</div>
-				{/* {end search} */}
-				{/* Tìm kiếm nâng cao */}
-				<div className='mt-2'>
-					{/* tìm kiếm input */}
-					<div className='md:flex md:flex-row md:justify-between'>
-						<div className='md:w-8/12 lg:w-9/12'>
-							{/* tác giả */}
-							<div className='mt-2'>
-								<label className='text-base font-bold'>Tác giả</label>
-								<input
-									type='text'
-									placeholder='Có thể bỏ trống.'
-									className=' mt-1 w-full flex-1 rounded border px-4 py-2  text-sm outline-none  md:py-3  md:text-base'
-								/>
-							</div>
-							{/* họa sĩ*/}
-							<div className='mt-2'>
-								<label className='text-base font-bold'>Họa sĩ</label>
-								<input
-									type='text'
-									placeholder='Có thể bỏ trống.'
-									className=' mt-1 w-full flex-1 rounded border px-4  py-2 text-sm outline-none  md:py-3  md:text-base'
-								/>
-							</div>
-						</div>
-						{/* tình trạng */}
-						<div className='mt-2'>
-							<label htmlFor='hoasi' className='text-base font-bold'>
-								Tình trạng
-							</label>
-							<select className='select select-accent select-sm mt-1 w-full max-w-xs md:select-md'>
-								<option defaultValue={0}>Tất cả</option>
-								<option value={1}>Đang tiến hành</option>
-								<option value={2}>Tạm ngưng</option>
-								<option value={3}>Hoàn thành</option>
-							</select>
+				<form>
+					{/* {seacrh} */}
+					<div className='relative mt-2 flex flex-col'>
+						<div className='flex w-full'>
+							<input
+								type='text'
+								placeholder='Tên Tác Phẩm...'
+								className=' w-full flex-1 rounded-l-md border px-4  py-2 text-sm outline-none md:py-3  md:text-base'
+							/>
+							<button
+								type='submit'
+								className='flex-grow-0 rounded-r-md bg-green-500 px-4 py-2 text-sm font-bold text-white  hover:bg-green-600  md:text-base'
+							>
+								Tìm kiếm
+							</button>
 						</div>
 					</div>
-					{/* tìm kiếm checkbox */}
+					{/* {end search} */}
+					{/* Tìm kiếm nâng cao */}
 					<div className='mt-2'>
-						<div className='text-base font-bold'>Thể loại</div>
+						{/* tìm kiếm input */}
+						<div className='md:flex md:flex-row md:justify-between'>
+							<div className='md:w-8/12 lg:w-9/12'>
+								{/* tác giả */}
+								<div className='mt-2'>
+									<label className='text-base font-bold'>Tác giả</label>
+									<input
+										type='text'
+										placeholder='Có thể bỏ trống.'
+										className=' mt-1 w-full flex-1 rounded border px-4 py-2  text-sm outline-none  md:py-3  md:text-base'
+									/>
+								</div>
+								{/* họa sĩ*/}
+								<div className='mt-2'>
+									<label className='text-base font-bold'>Họa sĩ</label>
+									<input
+										type='text'
+										placeholder='Có thể bỏ trống.'
+										className=' mt-1 w-full flex-1 rounded border px-4  py-2 text-sm outline-none  md:py-3  md:text-base'
+									/>
+								</div>
+							</div>
+							{/* tình trạng */}
+							<div className='mt-2'>
+								<label htmlFor='hoasi' className='text-base font-bold'>
+									Tình trạng
+								</label>
+								<select className='select select-accent select-sm mt-1 w-full max-w-xs md:select-md'>
+									<option defaultValue={0}>Tất cả</option>
+									<option value={1}>Đang tiến hành</option>
+									<option value={2}>Tạm ngưng</option>
+									<option value={3}>Hoàn thành</option>
+								</select>
+							</div>
+						</div>
+						{/* tìm kiếm checkbox */}
 						<div className='mt-2'>
-							{/* checkbox ngang 3 */}
-							<div className='grid grid-flow-row grid-cols-3 gap-4 md:grid-cols-4 lg:grid-cols-5'>
-								<div className='mb-4 flex items-center'>
-									<input
-										id='action'
-										type='checkbox'
-										className='h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600'
-									/>
-									<label
-										htmlFor='action'
-										className='ml-2 text-sm font-medium text-gray-900 dark:text-gray-300'
-									>
-										Action
-									</label>
-								</div>
-								<div className='mb-4 flex items-center'>
-									<input
-										id='action2'
-										type='checkbox'
-										className='h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600'
-									/>
-									<label
-										htmlFor='action2'
-										className='ml-2 text-sm font-medium text-gray-900 dark:text-gray-300'
-									>
-										Action2
-									</label>
-								</div>
-								<div className='mb-4 flex items-center'>
-									<input
-										id='action3'
-										type='checkbox'
-										className='h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600'
-									/>
-									<label
-										htmlFor='action3'
-										className='ml-2 text-sm font-medium text-gray-900 dark:text-gray-300'
-									>
-										Action3
-									</label>
+							<div className='text-base font-bold'>Thể loại</div>
+							<div className='mt-2'>
+								{/* checkbox ngang 3 */}
+								<div className='grid grid-flow-row grid-cols-3 gap-4 md:grid-cols-4 lg:grid-cols-5'>
+									<div className='mb-4 flex items-center'>
+										<input
+											id='action'
+											type='checkbox'
+											className='h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600'
+										/>
+										<label
+											htmlFor='action'
+											className='ml-2 text-sm font-medium text-gray-900 dark:text-gray-300'
+										>
+											Action
+										</label>
+									</div>
+									<div className='mb-4 flex items-center'>
+										<input
+											id='action2'
+											type='checkbox'
+											className='h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600'
+										/>
+										<label
+											htmlFor='action2'
+											className='ml-2 text-sm font-medium text-gray-900 dark:text-gray-300'
+										>
+											Action2
+										</label>
+									</div>
+									<div className='mb-4 flex items-center'>
+										<input
+											id='action3'
+											type='checkbox'
+											className='h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600'
+										/>
+										<label
+											htmlFor='action3'
+											className='ml-2 text-sm font-medium text-gray-900 dark:text-gray-300'
+										>
+											Action3
+										</label>
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-				</div>
+				</form>
+
 				{/* end Tìm kiếm nâng cao */}
 				<hr className='my-8 h-px  border-2 bg-gray-200  dark:bg-gray-700 ' />
 				<div className='mb-10 mt-3'>
