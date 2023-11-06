11065c7e128c8c4861be1d503dd8b35598e79c8d
quoc <108472981+kensoonv2@users.noreply.github.com>
2023-11-06 14:55:17 +0700

    update

--- resources/js/Pages/Admin/Components/SideBar.jsx
+++ resources/js/Pages/Admin/Components/SideBar.jsx
@@ -53,7 +53,7 @@ export default function SideBar() {
 									</details>
 								</li>
 								<li>
-									<a href='#'>Tài khoản</a>
+									<Link href={route('admin.user')}>Tài khoản</Link>
 								</li>
 								<li>
 									<Link href={route('admin.team')}>Nhóm dịch</Link>
@@ -163,87 +163,94 @@ export default function SideBar() {
 							</div>
 						</li>
 						<li className='group flex items-center gap-4 rounded px-3 py-4 hover:bg-blue-700 '>
-							<svg
-								xmlns='http://www.w3.org/2000/svg'
-								fill='none'
-								viewBox='0 0 24 24'
-								strokeWidth={1.5}
-								stroke='currentColor'
-								className='h-6 w-6 '
-							>
-								<path
-									strokeLinecap='round'
-									strokeLinejoin='round'
-									d='M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z'
-								/>
-							</svg>
-							<a
-								href='#'
+							<Link href={route('admin.user')}>
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									fill='none'
+									viewBox='0 0 24 24'
+									strokeWidth={1.5}
+									stroke='currentColor'
+									className='h-6 w-6 '
+								>
+									<path
+										strokeLinecap='round'
+										strokeLinejoin='round'
+										d='M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z'
+									/>
+								</svg>
+							</Link>
+							<Link
+								href={route('admin.user')}
 								className={`block  transition duration-200  group-hover:text-white ${!open && 'hidden'}`}
 							>
 								Tài khoản
-							</a>
+							</Link>
 						</li>
 						<li className='group flex items-center gap-4 rounded px-3 py-4 hover:bg-blue-700'>
-							<svg
-								xmlns='http://www.w3.org/2000/svg'
-								fill='none'
-								viewBox='0 0 24 24'
-								strokeWidth={1.5}
-								stroke='currentColor'
-								className='h-6 w-6'
-							>
-								<path
-									strokeLinecap='round'
-									strokeLinejoin='round'
-									d='M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25'
-								/>
-							</svg>
+							<Link href={route('admin.categories')}>
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									fill='none'
+									viewBox='0 0 24 24'
+									strokeWidth={1.5}
+									stroke='currentColor'
+									className='h-6 w-6'
+								>
+									<path
+										strokeLinecap='round'
+										strokeLinejoin='round'
+										d='M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25'
+									/>
+								</svg>
+							</Link>
 							<Link
-								className={`block transition duration-200 group-hover:text-white ${!open && 'hidden'}`}
 								href={route('admin.categories')}
+								className={`block transition duration-200 group-hover:text-white ${!open && 'hidden'}`}
 							>
 								Thể loại
 							</Link>
 						</li>
 						<li className='group flex items-center gap-4 rounded px-3 py-4 hover:bg-blue-700 '>
-							<svg
-								xmlns='http://www.w3.org/2000/svg'
-								fill='none'
-								viewBox='0 0 24 24'
-								strokeWidth={1.5}
-								stroke='currentColor'
-								className='h-6 w-6'
-							>
-								<path
-									strokeLinecap='round'
-									strokeLinejoin='round'
-									d='M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z'
-								/>
-							</svg>
+							<Link href={route('admin.team')}>
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									fill='none'
+									viewBox='0 0 24 24'
+									strokeWidth={1.5}
+									stroke='currentColor'
+									className='h-6 w-6'
+								>
+									<path
+										strokeLinecap='round'
+										strokeLinejoin='round'
+										d='M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z'
+									/>
+								</svg>
+							</Link>
 							<Link
-								className={`block transition duration-200 group-hover:text-white ${!open && 'hidden'}`}
 								href={route('admin.team')}
+								className={`block transition duration-200 group-hover:text-white ${!open && 'hidden'}`}
 							>
 								Nhóm dịch
 							</Link>
 						</li>
 						<li className='group flex items-center gap-4 rounded px-3 py-4 hover:bg-blue-700 '>
-							<svg
-								xmlns='http://www.w3.org/2000/svg'
-								fill='none'
-								viewBox='0 0 24 24'
-								strokeWidth={1.5}
-								stroke='currentColor'
-								className='h-6 w-6'
-							>
-								<path
-									strokeLinecap='round'
-									strokeLinejoin='round'
-									d='M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25'
-								/>
-							</svg>
-
+							<Link href={route('admin.novel')}>
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									fill='none'
+									viewBox='0 0 24 24'
+									strokeWidth={1.5}
+									stroke='currentColor'
+									className='h-6 w-6'
+								>
+									<path
+										strokeLinecap='round'
+										strokeLinejoin='round'
+										d='M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25'
+									/>
+								</svg>
+							</Link>
 							<Link
 								className={`block  transition duration-200  group-hover:text-white ${!open && 'hidden'}`}
 								href={route('admin.novel')}
@@ -277,20 +284,22 @@ export default function SideBar() {
 							</a>
 						</li>
 						<li className='group flex items-center gap-4 rounded px-3 py-4 hover:bg-blue-700'>
-							<svg
-								xmlns='http://www.w3.org/2000/svg'
-								fill='none'
-								viewBox='0 0 24 24'
-								strokeWidth={1.5}
-								stroke='currentColor'
-								className='h-6 w-6'
-							>
-								<path
-									strokeLinecap='round'
-									strokeLinejoin='round'
-									d='M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25'
-								/>
-							</svg>
+							<Link href='/'>
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									fill='none'
+									viewBox='0 0 24 24'
+									strokeWidth={1.5}
+									stroke='currentColor'
+									className='h-6 w-6'
+								>
+									<path
+										strokeLinecap='round'
+										strokeLinejoin='round'
+										d='M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25'
+									/>
+								</svg>
+							</Link>
 							<Link
 								href='/'
 								className={`block transition duration-200 group-hover:text-white ${!open && 'hidden'}`}
