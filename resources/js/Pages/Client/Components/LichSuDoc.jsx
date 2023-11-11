2ed80ceac13ee85b9c8aa5be1dfe9f8560e1db52
manden520 <92621988+manden520@users.noreply.github.com>
2023-11-11 14:29:13 +0700

    up

--- resources/js/Pages/Client/Components/LichSuDoc.jsx
+++ resources/js/Pages/Client/Components/LichSuDoc.jsx
@@ -5,34 +5,38 @@ export default function LichSuDoc({ historyReadList }) {
 			<div className='mb-2'>
 				<h1 className='font-bold uppercase text-black'>Truyện vừa đọc</h1>
 			</div>
-			<div className='bottom relative'>
-				{historyReadList.map((item, index) => (
-					<div key={index} className='mb-2 flex flex-row gap-2'>
-						{/* Thumbnail */}
-						<div className='w-3/12'>
-							<img
-								className='h-32 w-full object-cover object-center md:h-40 lg:h-44'
-								src={item.novel.thumbnail}
-								alt='thumbnail'
-							/>
-						</div>
-						{/* Content */}
-						<div className='w-9/12'>
-							<Link href={`/novel/${item.novel.slug}`} className='text-xs sm:text-base lg:text-xl'>
-								{item.novel.name_novel}
-							</Link>
-							<div>{item.chap.vol.title}</div>
-							<div>
-								<Link
-									href={`/novel/${item.novel.slug}/${item.chap.vol.slug}/${item.chap.slug}`}
-									className='text-xs sm:text-base'
-								>
-									{item.chap.title}
-								</Link>
-							</div>
-						</div>
-					</div>
-				))}
+			<div className='bottom relative h-96 overflow-x-auto'>
+				<table className='table table-pin-rows'>
+					<tbody>
+						{historyReadList.map((item, index) => (
+							<tr key={index} className='mb-2 flex flex-row gap-2'>
+								{/* Thumbnail */}
+								<div className='w-3/12'>
+									<img
+										className='h-32 w-full object-cover object-center md:h-40 lg:h-44'
+										src={item.novel.thumbnail}
+										alt='thumbnail'
+									/>
+								</div>
+								{/* Content */}
+								<div className='w-9/12'>
+									<Link href={`/novel/${item.novel.slug}`} className='text-xs sm:text-base lg:text-xl'>
+										{item.novel.name_novel}
+									</Link>
+									<div>{item.chap.vol.title}</div>
+									<div>
+										<Link
+											href={`/novel/${item.novel.slug}/${item.chap.vol.slug}/${item.chap.slug}`}
+											className='text-xs sm:text-base'
+										>
+											{item.chap.title}
+										</Link>
+									</div>
+								</div>
+							</tr>
+						))}
+					</tbody>
+				</table>
 			</div>
 		</div>
 	);
