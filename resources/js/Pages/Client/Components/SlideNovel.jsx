91d98a088995fd56c8400d674560ec05f320c3a7
Trung Khiem <trungkhiem1412@gmail.com>
2023-11-12 13:01:42 +0700

    Add random novel slider to home page

--- resources/js/Pages/Client/Components/SlideNovel.jsx
+++ resources/js/Pages/Client/Components/SlideNovel.jsx
@@ -1,19 +1,48 @@
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
 export default function SlideNovel({ novels }) {
+	const [arrNovel, setArrNovel] = useState(novels.data);
+	arrNovel.sort(function () {
+		return 0.5 - Math.random();
+	});
+	const arrNovelRandom = arrNovel.slice(0, 4);
 	return (
-		<div className='carousel w-full'>
-			{novels.data.map((item, index) => (
-				<div id={'slide' + index} key={index} className='carousel-item relative w-full'>
-					<img src={item.thumbnail} className='w-full' />
-					<div className='absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between'>
-						<a href={'#slide' + (index - 1)} className='btn btn-circle'>
-							❮
-						</a>
-						<a href={'#slide' + (index + 1)} className='btn btn-circle'>
-							❯
-						</a>
-					</div>
-				</div>
-			))}
-		</div>
+		<>
+			Truyện Ngẫu Nhiên
+			<Swiper
+				pagination={{
+					dynamicBullets: true,
+				}}
+				modules={[Pagination]}
+				className='mySwiper'
+			>
+				{arrNovelRandom.map((novel, index) => (
+					<SwiperSlide key={index}>
+						<div className='shadow-md'>
+							<div className='flex h-52 gap-3 rounded-md border p-1 lg:h-72'>
+								{/* thumbnail */}
+								<div className='thumbnail w-4/12 '>
+									<img className='h-full w-full object-contain' src={novel.thumbnail} alt='' />
+								</div>
+								{/* content */}
+								<div className='content w-8/12'>
+									<div className='title text-xl font-semibold uppercase'>{novel.name_novel}</div>
+									<div className='author'>{novel.author}</div>
+									<div className='description'>{novel.description}</div>
+								</div>
+							</div>
+						</div>
+					</SwiperSlide>
+				))}
+			</Swiper>
+		</>
 	);
 }
