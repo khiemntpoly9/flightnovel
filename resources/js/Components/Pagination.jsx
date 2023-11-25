86267910335fe88aeb81455e907a0453c37dceb6
Trung Khiem <trungkhiem1412@gmail.com>
2023-11-25 14:41:44 +0700

    Refactor pagination component

--- resources/js/Components/Pagination.jsx
+++ resources/js/Components/Pagination.jsx
@@ -1,6 +1,6 @@
 import { Link } from '@inertiajs/react';
+import { router } from '@inertiajs/react';
 export default function Pagination({ links }) {
-	console.log(links);
 	return (
 		<div>
 			<div className='mb-1 mt-2 flex content-center justify-center gap-1'>
@@ -12,11 +12,18 @@ export default function Pagination({ links }) {
 								dangerouslySetInnerHTML={{ __html: link.label }}
 							/>
 						) : (
-							<Link
+							// <Link
+							// 	className={`rounded border px-4 py-3 text-sm leading-4 ${
+							// 		link.active ? 'bg-red-500 text-white' : 'text-gray-400 hover:bg-white'
+							// 	}`}
+							// 	href={`${link.url}`}
+							// 	dangerouslySetInnerHTML={{ __html: link.label }}
+							// />
+							<button
 								className={`rounded border px-4 py-3 text-sm leading-4 ${
 									link.active ? 'bg-red-500 text-white' : 'text-gray-400 hover:bg-white'
 								}`}
-								href={`${link.url}`}
+								onClick={() => router.get(link.url)}
 								dangerouslySetInnerHTML={{ __html: link.label }}
 							/>
 						)}
