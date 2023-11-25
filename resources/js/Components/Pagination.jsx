364f08d5e59f4e08e311b1b035b0c6594aaaa899
Trung Khiem <trungkhiem1412@gmail.com>
2023-11-25 14:06:56 +0700

    Fix link URL in Pagination component

--- resources/js/Components/Pagination.jsx
+++ resources/js/Components/Pagination.jsx
@@ -1,5 +1,6 @@
 import { Link } from '@inertiajs/react';
 export default function Pagination({ links }) {
+	console.log(links);
 	return (
 		<div>
 			<div className='mb-1 mt-2 flex content-center justify-center gap-1'>
@@ -15,7 +16,7 @@ export default function Pagination({ links }) {
 								className={`rounded border px-4 py-3 text-sm leading-4 ${
 									link.active ? 'bg-red-500 text-white' : 'text-gray-400 hover:bg-white'
 								}`}
-								href={`https://${link.url}`}
+								href={`${link.url}`}
 								dangerouslySetInnerHTML={{ __html: link.label }}
 							/>
 						)}
