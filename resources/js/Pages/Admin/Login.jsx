32a9b8ff01a489c8f91cb97c363ac1cc7f97749b
Trung Khiem <trungkhiem1412@gmail.com>
2023-11-26 22:14:50 +0700

    Remove unused TextInput component from LoginAdmin component

--- resources/js/Pages/Admin/Login.jsx
+++ resources/js/Pages/Admin/Login.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import { Head, Link, useForm } from '@inertiajs/react';
-import TextInput from '@/Components/TextInput';
 import { FaGoogle } from 'react-icons/fa';
 
 export default function LoginAdmin() {
@@ -12,10 +11,6 @@ export default function LoginAdmin() {
 
 	const [showPassword, setShowPassword] = useState(false);
 
-	const toggleShowPassword = () => {
-		setShowPassword(!showPassword);
-	};
-
 	useEffect(() => {
 		return () => {
 			reset('password');
@@ -26,6 +21,7 @@ export default function LoginAdmin() {
 		e.preventDefault();
 		post(route('login.admin'));
 	};
+
 	return (
 		<div className='container mx-auto w-10/12 bg-white'>
 			<Head title='Đăng nhập Admin' />
@@ -41,7 +37,7 @@ export default function LoginAdmin() {
 								Tài khoản
 							</label>
 							<div>
-								<TextInput
+								<input
 									id='email'
 									type='email'
 									name='email'
@@ -50,7 +46,6 @@ export default function LoginAdmin() {
 										errors && errors.email ? 'mb-2 border-rose-600' : ''
 									} w-full appearance-none rounded border p-2 shadow focus:outline-none`}
 									autoComplete='username'
-									isFocused={true}
 									onChange={(e) => setData('email', e.target.value)}
 								/>
 								{errors && errors.email && <p className='text-sm italic text-red-500'>{errors.email}</p>}
@@ -71,44 +66,59 @@ export default function LoginAdmin() {
 								</div>
 							</div>
 							<div className='mt-2'>
-								<TextInput
-									id='password'
-									type={showPassword ? 'text' : 'password'}
-									name='password'
-									value={data.password}
-									className={`${
+								<div
+									className={`flex gap-1 ${
 										errors && errors.password ? 'mb-2 border-rose-600' : ''
 									} w-full appearance-none rounded border p-2 shadow focus:outline-none`}
-									autoComplete='current-password'
-									onChange={(e) => setData('password', e.target.value)}
-								/>
-								<TextInput
-									type='checkbox'
-									id='showPassword'
-									checked={showPassword}
-									onChange={toggleShowPassword}
-									className='hidden '
-								/>
-								<label
-									htmlFor='showPassword'
-									className='absolute translate-x-1 translate-y-1/2 transform cursor-pointer'
 								>
-									<svg
-										xmlns='http://www.w3.org/2000/svg'
-										fill='none'
-										viewBox='0 0 24 24'
-										strokeWidth={1.5}
-										stroke='currentColor'
-										className='h-6 w-6'
-									>
-										<path
-											strokeLinecap='round'
-											strokeLinejoin='round'
-											d='M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z'
-										/>
-										<path strokeLinecap='round' strokeLinejoin='round' d='M15 12a3 3 0 11-6 0 3 3 0 016 0z' />
-									</svg>
-								</label>
+									<input
+										id='password'
+										type={showPassword ? 'text' : 'password'}
+										name='password'
+										value={data.password}
+										className='w-full border-none outline-none focus:outline-none'
+										autoComplete='current-password'
+										onChange={(e) => setData('password', e.target.value)}
+									/>
+									<div onClick={() => setShowPassword(!showPassword)}>
+										{showPassword ? (
+											<svg
+												xmlns='http://www.w3.org/2000/svg'
+												fill='none'
+												viewBox='0 0 24 24'
+												strokeWidth={1.5}
+												stroke='currentColor'
+												className='h-6 w-6'
+											>
+												<path
+													strokeLinecap='round'
+													strokeLinejoin='round'
+													d='M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88'
+												/>
+											</svg>
+										) : (
+											<svg
+												xmlns='http://www.w3.org/2000/svg'
+												fill='none'
+												viewBox='0 0 24 24'
+												strokeWidth={1.5}
+												stroke='currentColor'
+												className='h-6 w-6'
+											>
+												<path
+													strokeLinecap='round'
+													strokeLinejoin='round'
+													d='M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z'
+												/>
+												<path
+													strokeLinecap='round'
+													strokeLinejoin='round'
+													d='M15 12a3 3 0 11-6 0 3 3 0 016 0z'
+												/>
+											</svg>
+										)}
+									</div>
+								</div>
 								{errors && errors.password && (
 									<p className='text-sm italic text-red-500'>{errors.password}</p>
 								)}
