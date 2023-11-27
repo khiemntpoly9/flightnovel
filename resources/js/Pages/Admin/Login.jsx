336ed7680c43787ee9193847c6ad127c42c3657a
quoc <108472981+kensoonv2@users.noreply.github.com>
2023-11-27 14:15:13 +0700

    update

--- resources/js/Pages/Admin/Login.jsx
+++ resources/js/Pages/Admin/Login.jsx
@@ -30,8 +30,10 @@ export default function LoginAdmin() {
 			</div>
 			<div className='flex min-h-full flex-1 flex-col justify-center lg:px-8'>
 				<div className='sm:mx-auto sm:w-full sm:max-w-sm'>
-					<h2 className='text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>
-						Đăng nhập admin
+					<h2 className='text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>Đăng nhập</h2>
+					<h2 className='text-center text-2xl font-bold uppercase leading-9 tracking-tight text-gray-900'>
+						{' '}
+						admin
 					</h2>
 				</div>
 				<div className='mt-2 p-8 shadow-2xl sm:mx-auto sm:w-full sm:max-w-sm'>
