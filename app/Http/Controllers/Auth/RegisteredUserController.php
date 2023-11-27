d0c1f212d131a7d7d0b72f728734b985354b5c1c
Trung Khiem <trungkhiem1412@gmail.com>
2023-11-27 12:36:56 +0700

    Fix user authentication and email verification

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -10,7 +10,6 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules;
 use Inertia\Inertia;
 use Inertia\Response;
 use Illuminate\Support\Str;
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,23 +4,35 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Lang;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The model to policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
-    protected $policies = [
-        //
-    ];
+	/**
+	 * The model to policy mappings for the application.
+	 *
+	 * @var array<class-string, class-string>
+	 */
+	protected $policies = [
+		//
+	];
 
-    /**
-     * Register any authentication / authorization services.
-     */
-    public function boot(): void
-    {
-        //
-    }
+	/**
+	 * Register any authentication / authorization services.
+	 */
+	public function boot(): void
+	{
+		VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
+			return (new MailMessage)
+				->subject(Lang::get('Xác minh tài khoản'))
+				->greeting(Lang::get('Xin chào!'))
+				->line(Lang::get('Bạn đã đăng ký tài khoản thành công.'))
+				->line(Lang::get('Bấm vào nút sau để xác minh tài khoản của bạn.'))
+				->action(Lang::get('Xác minh tài khoản'), $url)
+				->line(Lang::get('Nếu bạn không đăng ký tài khoản, vui lòng bỏ qua email này.'))
+				->salutation(Lang::get('Trân trọng, ' . config('app.name') . '.'));
+		});
+	}
 }
